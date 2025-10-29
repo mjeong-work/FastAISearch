9b7204181ab24a4eec261eeef3f0b2c14631d7f7
mjeong-work <jmk0804@gmail.com>
2025-10-29 16:22:11 +0000

    Restore from the error of the upper floating bar update

--- main.py
+++ main.py
@@ -89,6 +89,3 @@ async def get_categories():
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
